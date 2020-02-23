167a124d0659f0fc84a8d97d3442b4fdc6b68f7e
Luis Felipe Bertel M <ellipe@gmail.com>
2020-02-23 10:13:22 -0500

    filters and sorting

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -4,11 +4,15 @@
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
           <PxIcon class="mr-4" />
-          <router-link :to="{name: 'home'}">
-            <span class="font-semibold text-xl tracking-tight">PlatziExchange</span>
+          <router-link :to="{ name: 'home' }">
+            <span class="font-semibold text-xl tracking-tight"
+              >PlatziExchange</span
+            >
           </router-link>
         </div>
-        <div class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div
+          class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto"
+        >
           <div class="text-sm lg:flex-grow"></div>
         </div>
       </nav>
